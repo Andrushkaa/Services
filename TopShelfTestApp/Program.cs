f37e5972ffcb3a84dd40d8c041e6fa914afa2343
Andrushkaa <andrij4ik+1@gmail.com>
2016-01-06 20:35:06 +0200

    Initial commit

---
+++ TopShelfTestApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace TopShelfTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(hostConfig =>
+            {
+                hostConfig.Service<Service>(serviseConfig =>
+                {
+                    serviseConfig.ConstructUsing(() => new Service());
+                    serviseConfig.WhenStarted(service => service.Start());
+                    serviseConfig.WhenStopped(service => service.Stop());
+                });
+                hostConfig.RunAsLocalSystem();
+
+                hostConfig.SetDisplayName("Service");
+                hostConfig.SetDescription("Service using TopShelf");
+                hostConfig.SetServiceName("Service");
+            });
+        }
+    }
+}
---
+++ TopShelfTestApp/Service.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopShelfTestApp
+{
+    class Service
+    {
+        public void Start()
+        {
+
+        }
+        public void Stop()
+        {
+
+        }
+    }
+}
